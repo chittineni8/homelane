2ec991d38b0510d6bba7fc425460f01f57f76d25
Manav_Padhariya <manav.p@codilar.com>
2021-09-29 15:13:58 +0530

    [customer/lastname]

---
+++ app/code/Codilar/TokenAPI/Setup/Patch/Data/UpdateLastnameAttribute.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Codilar\TokenAPI\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+/**
+ * @package     DanubeHome PWA
+ * @author        Codilar Technologies
+ * @link              http://www.codilar.com/
+ * @copyright © 2021 Codilar Technologies Pvt. Ltd.. All rights reserved.
+ */
+class UpdateLastnameAttribute implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected $moduleDataSetup;
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->updateAttribute(
+                Customer::ENTITY,
+                    'lastname',
+                    'is_required',
+                    false
+            );
+        $this->moduleDataSetup->getConnection()->endSetup();
+        return $this;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getVersion()
+    {
+        return '2.0.14';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
