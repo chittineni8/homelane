58894e07c17e4d613ed7b7ead7f51ec90b6182d9
Shahed <shahed@codilar.com>
2021-10-06 15:51:47 +0530

    review media url fix

--- app/code/Codilar/Reviews/Block/Widget/PageWidget.php
+++ app/code/Codilar/Reviews/Block/Widget/PageWidget.php
@@ -3,12 +3,38 @@
 namespace Codilar\Reviews\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Widget\Block\BlockInterface;
-
 
 class PageWidget extends Template implements BlockInterface
 {
-
     protected $_template = "Codilar_Reviews::widget/page_widget.phtml";
-}
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
 
+    /**
+     * PageWidget constructor.
+     * @param StoreManagerInterface $storeManager
+     * @param Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        Template\Context $context,
+        array $data = []
+    ) {
+
+        parent::__construct($context, $data);
+        $this->storeManager = $storeManager;
+    }
+
+    public function getMediaUrl()
+    {
+        $mediaUrl = $this->_storeManager
+            ->getStore()
+            ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+        return $mediaUrl;
+    }
+}
