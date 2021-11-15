4492210e46156e16d11f79d8ef2dad4db8c9827c
root <abhinav.v@codilar.com>
2021-11-15 08:58:43 +0000

    if product in wishlist heart will be in red color for that specific  product

---
+++ app/code/Codilar/WishlistHighlighter/Block/Product/ListProduct.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ *
+ * @package
+ * @author
+ *
+
+ */
+
+namespace Codilar\WishlistHighlighter\Block\Product;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Wishlist\Model\ResourceModel\Item\CollectionFactory;
+
+class ListProduct extends Template
+
+{
+    protected $customer;
+    /**
+     *
+     * @var StoreManagerInterface
+     */
+    protected $storemanagerinterface;
+
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $_wishlistCollectionFactory;
+
+    public function __construct(
+        CollectionFactory                    $wishlistCollectionFactory,
+        StoreManagerInterface                $storemanagerinterface,
+        \Magento\Customer\Model\Session      $customer,
+        \Codilar\Customer\Block\CustomerData $customerdata,
+        \Magento\Wishlist\Model\Wishlist     $wishlist
+    )
+    {
+        $this->customer = $customer;
+        $this->customerdata = $customerdata;
+        $this->wishlist = $wishlist;
+        $this->_wishlistCollectionFactory = $wishlistCollectionFactory;
+        $this->storemanagerinterface = $storemanagerinterface;
+
+    }
+
+    public function currentCustomer()
+    {
+
+        return $customer = $this->customerdata->getCustomerId();
+
+
+    }
+
+    public function getWishlistByCustomerId()
+    {
+        $customerId = $this->currentCustomer();
+
+        $collection = $this->_wishlistCollectionFactory->create()->addCustomerIdFilter($customerId);
+        $baseurl = $this->storemanagerinterface->getStore()->getBaseUrl();
+
+        $wishlistData = [];
+        foreach ($collection as $item) {
+            $productInfo = $item->getProduct()->toArray();
+            $data = [
+
+                'product_id' => $item->getProductId(),
+
+            ];
+            $wishlistData[] = $data;
+        }//end foreach
+        return $wishlistData;
+    }
+
+
+}
