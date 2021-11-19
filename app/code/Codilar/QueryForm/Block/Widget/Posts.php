d2b0993fc853bb20c60986ca26d86a709aa23fd5
CHANDRASEKHAR613 <sekhar619.india@gmail.com>
2021-11-19 15:31:10 +0530

    getting baseurl

--- app/code/Codilar/QueryForm/Block/Widget/Posts.php
+++ app/code/Codilar/QueryForm/Block/Widget/Posts.php
@@ -3,16 +3,47 @@ namespace Codilar\QueryForm\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\Template\Context;
 
 
 class Posts extends Template
 {
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    protected $urlInterface;
+
     protected $_template = "widget/posts.phtml";
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        UrlInterface $urlInterface,
+        Context $context,
+        array   $data = []
+    )
+    
+    {   $this->storeManager = $storeManager;
+        $this->urlInterface = $urlInterface;
+        parent::__construct($context, $data);
+
+    }//end __construct()
+
+
 
     public function getPostUrl()
     {
         return $this->getUrl('query/query/save');
     }
 
+    public function getBaseUrl()
+    {
+        return  $storeUrl = $this->storeManager->getStore()->getBaseUrl();
+    }
 
 }
