b1e8d9c586eb9a12e0a5e9bc1b870bb19324a1c0
root <abhinav.v@codilar.com>
2021-11-22 11:31:50 +0000

    store filter added when getting collection of wishlist items AJAX

--- app/code/Codilar/WishlistAPI/Model/Api/WishlistManagement.php
+++ app/code/Codilar/WishlistAPI/Model/Api/WishlistManagement.php
@@ -1,6 +1,9 @@
 <?php
 /**
- *
+ * @package   Codilar\WishlistAPI
+ * @author    Abhinav Vinayak <abhinav.v@codilar.com>
+ * @copyright © 2021 Codilar
+ * @license   See LICENSE file for license details.
  *
  */
 
@@ -143,11 +146,23 @@ class WishlistManagement implements WishlistManagementInterface
 
     /**
      * @param CollectionFactory $wishlistCollectionFactory
-     * @param ProductFactory $productFactory
-     * @param \Magento\Framework\Math\Random $mathRandom
+     * @param WishlistFactory $wishlistFactory
+     * @param Http $http
+     * @param TokenFactory $tokenFactory
+     * @param RequestInterface $request
      * @param Customer $customer
-     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param AppEmulation $appEmulation
+     * @param CountryFactory $countryfactory
+     * @param JsonFactory $jsonFactory
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param StoreManagerInterface $storemanagerinterface
+     * @param ProductImageHelper $productImageHelper
+     * @param CustomerCollection $customerCollection
+     * @param Product $productload
+     * @param Logger $loggerResponse
      * @param ProductRepositoryInterface $productRepository
+     * @param ItemFactory $itemFactory
      */
     public function __construct(
         CollectionFactory           $wishlistCollectionFactory,
@@ -313,7 +328,7 @@ class WishlistManagement implements WishlistManagementInterface
                             return $wishlistDeleteResponse;
                         }
                     }
-                        
+
 
                 } else {
 
--- app/code/Codilar/WishlistHighlighter/Controller/Index/WishlistHeart.php
+++ app/code/Codilar/WishlistHighlighter/Controller/Index/WishlistHeart.php
@@ -11,25 +11,56 @@
 
 namespace Codilar\WishlistHighlighter\Controller\Index;
 
-class WishlistHeart extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Wishlist\Helper\Data;
+
+class WishlistHeart extends Action
 {
 
+    /**
+     * @param Context $context
+     * @param Data $wishlistHelper
+     * @param StoreManagerInterface $storeManager
+     * @param JsonFactory $jsonFactory
+     */
     public function __construct(
-        \Magento\Framework\App\Action\Context            $context,
-        \Magento\Wishlist\Helper\Data                    $wishlistHelper,
-        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+        Context            $context,
+        Data                    $wishlistHelper,
+        StoreManagerInterface       $storeManager,
+        JsonFactory $jsonFactory
     )
     {
-        parent::__construct($context);
+
         $this->wishlistHelper = $wishlistHelper;
         $this->jsonFactory = $jsonFactory;
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
     }
 
+    /**
+     * @return ResponseInterface|Json|ResultInterface
+     */
     public function execute()
     {
         $result = $this->jsonFactory->create();
-        $data = $this->wishlistHelper->getWishlistItemCollection()->getData();
+        $data = $this->wishlistHelper->getWishlistItemCollection()->addFieldToFilter('store_id', $this->getStoreId())->getData();
 
         return $result->setData(['status' => 200, 'items' => $data]);
+    }
+
+    /**
+     * @return int
+     * @throws NoSuchEntityException
+     */
+    public function getStoreId()
+    {
+        return $this->storeManager->getStore()->getId();
     }
 }
