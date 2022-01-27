30cd59577a5a4a8d420c0abc404ae1c696f8ec88
root <chandrasekhar.kd@codilar.com>
2022-01-27 18:53:47 +0530

    discount percentage in wishlist

--- app/code/Codilar/CategoryFilter/ViewModel/Category.php
+++ app/code/Codilar/CategoryFilter/ViewModel/Category.php
@@ -8,10 +8,14 @@ use \Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Widget\Block\BlockInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-Class Category implements ArgumentInterface
+use Magento\Catalog\Model\ProductFactory;
+
+class Category implements ArgumentInterface
 {
+    protected $productFactory;
+
     protected $url;
-   
+
     /**
      * @var \Magento\Catalog\Model\CategoryFactory
      */
@@ -28,39 +32,55 @@ Class Category implements ArgumentInterface
     protected $_storeManager;
 
     /**
-     * @param Template\Context                                        $context
-     * @param \Magento\Catalog\Model\CategoryFactory                  $_categoryFactory
+     * @param Template\Context $context
+     * @param \Magento\Catalog\Model\CategoryFactory $_categoryFactory
      * @param \Magento\Catalog\Model\CategoryRepository;              $categoryRepository
      * @param \Magento\Store\Model\StoreManagerInterface;             $_storeManager
-     * @param array                                                   $data
+     * @param array $data
      */
-    
 
 
     public function __construct(
-                 \Magento\Framework\UrlInterface $url,
-                 \Magento\Store\Model\StoreManagerInterface $storeManager,
-                 \Magento\Catalog\Model\CategoryRepository $categoryRepository
-                // \Magento\Catalog\Model\CategoryFactory $categoryFactory,
-                // \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collecionFactory
-            )
-            {
-                $this->url                       = $url;
-                $this->_storeManager             = $storeManager;
-                $this->categoryRepository        = $categoryRepository;
-                // $this->_categoryFactory          = $categoryFactory;
-                // $this->_collectionFactory        = $collecionFactory;
-            }
+        ProductFactory                             $productFactory,
+        \Magento\Framework\UrlInterface            $url,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\CategoryRepository  $categoryRepository
+        // \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        // \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collecionFactory
+    )
+    {
+        $this->productFactory = $productFactory;
+        $this->url = $url;
+        $this->_storeManager = $storeManager;
+        $this->categoryRepository = $categoryRepository;
+        // $this->_categoryFactory          = $categoryFactory;
+        // $this->_collectionFactory        = $collecionFactory;
+    }
 
-     public function getCategoryUrl($categoryId)
-     {
-        if(empty($categoryId)){
-              
+    public function getCategoryUrl($categoryId)
+    {
+        if (empty($categoryId)) {
+
             $categoryId = 8;
         }
         $category = $this->categoryRepository->get($categoryId, $this->_storeManager->getStore()->getId());
 
         return $category->getUrl();
 
-      }
-}      
+    }
+
+    public function getDiscountPercents($id)
+    {
+        $product = $this->productFactory->create()->load($id);
+        $productPrice = $product->getPrice();
+        $productFinalPrice = $product->getFinalPrice();
+        if ($productFinalPrice < $productPrice):
+            $_Percent = 100 - round(($productFinalPrice / $productPrice) * 100);
+            return $_Percent;
+        else:
+            return null;
+        endif;
+
+    }
+
+}
