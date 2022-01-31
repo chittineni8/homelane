2ca9e4b352d360caf2d945b4b50ff8c15b0f9bca
root <abhinav.v@codilar.com>
2022-01-31 17:50:56 +0530

    wasated attribute module

---
+++ app/code/Codilar/Wastage/Setup/Patch/Data/Wastage.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace Codilar\Wastage\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class Wastage implements DataPatchInterface
+{
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * AddRecommendedAttribute constructor.
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory          $eavSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute('catalog_product', 'serving_wastage_percentage', [
+            'type' => 'text',
+            'label' => 'Serving Area and Wastage Percentage',
+            'input' => 'text',
+            'source' => '',
+            'default' => '',
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'visible' => true,
+            'used_in_product_listing' => true,
+            'user_defined' => true,
+            'required' => false,
+            'group' => 'General',
+            'sort_order' => 100,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
